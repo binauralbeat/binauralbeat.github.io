206e7e654c81512680dddde8df135b84662d40f3
dan beecroft <binauralbeat001@gmail.com>
2018-07-06 15:32:18 -0500

    trying to fix

---
+++ scripts/nav.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    let navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "bar" + " card" + " animate-top" + " white";
+    } else {
+        navbar.className = navbar.className.replace(" card animate-top white", "");
+    }
+}

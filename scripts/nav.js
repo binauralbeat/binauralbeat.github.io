28b00543cbe1dd3a161ffa4bbb891cf195f4e536
dan beecroft <binauralbeat001@gmail.com>
2018-12-12 18:47:54 -0600

    styling changes, logo, body color

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,6 +4,6 @@ function myFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "bar" + " card" + " animate-top" + " white";
     } else {
-        navbar.className = navbar.className.replace(" card animate-top white", "");
+        navbar.className = navbar.className.replace(" card" + "animate-top" + "white", "");
     }
 }
